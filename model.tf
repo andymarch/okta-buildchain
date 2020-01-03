184f000deac0787687b98b4d9322bf193f1b774f
Andy March <mail@andymarch.co.uk>
2020-01-03 15:13:39 +0000

    Init commit

---
+++ model.tf
@@ -0,0 +1,32 @@
+variable "org_name" {}
+variable "api_token" {}
+variable "base_url" {}
+
+data "okta_group" "all" {
+  name = "Everyone"
+}
+
+provider "okta" { 
+    org_name = "${var.org_name}"
+    base_url = "${var.base_url}"
+    api_token = "${var.api_token}"
+}
+
+//extension to schema must be created one at a time, use depends_on to order the
+//creation of schema extensions
+
+resource "okta_user_schema" "dob_extension" {
+  index  = "date_of_birth"
+  title  = "Date of Birth"
+  type   = "string"
+  master = "PROFILE_MASTER"
+  required = true  
+}
+resource "okta_user_schema" "crn_extension" {
+  index  = "customer_reference_number"
+  title  = "Customer Reference Number"
+  required = true
+  type   = "string"
+  master = "PROFILE_MASTER"
+  depends_on = ["okta_user_schema.dob_extension"]
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Okta Build Chain Demonstration
+
+This repository is attached to a build chain to control the configuration of a
+number of Okta instances during the development process.
+
+# Okta Terraform Provider
+
+This demonstration makes use of the Okta Terraform provider to configure the
+individual instances of Okta. Read more about the provider [here](https://www.terraform.io/docs/providers/okta/index.html).
